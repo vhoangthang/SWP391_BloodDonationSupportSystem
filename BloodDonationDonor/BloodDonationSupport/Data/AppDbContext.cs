d865d2cdf383751baf05fa50733ffb520e8bb921
Hoang Thang <thangvhse182121@fpt.edu.vn>
2025-06-20 18:25:34 +0700

    add Donor program

---
+++ BloodDonationDonor/BloodDonationSupport/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using BloodDonationSupport.Models;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace BloodDonationSupport.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options) { }
+
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Donor> Donor { get; set; }
+        public DbSet<BloodType> BloodTypes { get; set; }
+        public DbSet<DonationAppointment> DonationAppointments { get; set; }
+
+        public DbSet<HealthSurvey> HealthSurveys { get; set; }
+
+        public DbSet<MedicalCenter> MedicalCenters { get; set; }
+    }
+}
---
+++ BloodDonationDonor/BloodDonationSupport/Models/BloodType.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BloodDonationSupport.Models
+{
+    [Table("BloodType")]
+    public class BloodType
+    {
+        public int BloodTypeID { get; set; }
+        public string Type { get; set; }
+    }
+}
