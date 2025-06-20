65d58f432c5b8e05bd82b7d93863652c73a877c4
Hoang Thang <thangvhse182121@fpt.edu.vn>
2025-06-20 18:21:44 +0700

    add Donor program

---
+++ BloodDonationDonor/BloodDonationSupport/BloodDonationSupport/wwwroot/js/DonationBookingForm.js
@@ -0,0 +1,44 @@
+﻿document.getElementById('donationForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+    const formData = {};
+    const form = e.target;
+
+    // Lấy tất cả input radio đã chọn
+    const checkedInputs = form.querySelectorAll('input[type="radio"]:checked');
+    checkedInputs.forEach(input => {
+        formData[input.name] = input.value;
+    });
+
+    // Kiểm tra các trường hợp không đủ điều kiện hiến máu
+    if (
+        formData["3_TruocDayAnhChiCoMacCacBenhLietKeKhong"] === "true" ||
+        formData["4_KhoiBenhSauMacCacBenh12Thang"] === "true" ||
+        formData["4_DuocTruyenMauHoacGayGhepMo"] === "true" ||
+        formData["4_TiemVaccine"] === "true" ||
+        formData["5_KhoiBenhSauMacCacBenh6Thang"] === "true"
+    ) {
+        alert("Bạn không đủ điều kiện hiến máu do có tiền sử bệnh lý không phù hợp. Đăng ký sẽ bị huỷ. Bạn sẽ được chuyển về trang chủ.");
+        window.location.href = "/";
+        return;
+    }
+
+    fetch('/DonationForm/SubmitSurvey', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ answers: formData })
+    })
+        .then(res => res.json())
+        .then(data => {
+            if (data.success) {
+                alert("Đăng ký thành công!");
+                window.location.href = '/DonationSummary/Index?appointmentId=' + data.appointmentId;
+            } else {
+                alert("Lỗi: " + data.message);
+                if (data.message && data.message.includes("14 ngày")) {
+                    window.location.href = '/';
+                }
+            }
+        });
+});
